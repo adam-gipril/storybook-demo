99ddeb43096e728aac6b98ce0912b19315af05fc
Adam Gipril <adam.gipril@icloud.com>
2020-02-20 05:27:23 -0700

    Add storybook addon-knobs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import '../src/index.css';
 
 addDecorator(story => <div style={{ margin: '15px' }}>{story()}</div>);
+addDecorator(withKnobs({ escapeHTML: false }));
